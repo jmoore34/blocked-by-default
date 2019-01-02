2197e8adaf47d752600fc482e95f900e22875537
jon <programjm@gmail.com>
2019-01-02 00:25:51 -0600

    Refactor currentTab -> currentDomain

--- public/background.js
+++ public/background.js
@@ -1,14 +1,20 @@
 // @flow
 import { SITES } from 'constants'
 import Site from 'Site';
-//What tab is currently active.
-// LI: Is equivalent  to the current tab.
-// When the current tab changes, currentTab has the old tab until tabs.onUpdated event handler runs completely
-let currentTab = null;
+
+
+//What domain is currently active.
+// LI: Is equivalent  to the current domain.
+// When the current domain changes, currentTab has the old domain until domains.onUpdated event handler runs completely
+let currentDomain = null;
+
+// On tab change:
 chrome.tabs.onUpdated.addListener(
     (tabId, changeInfo, tab) => {
         alert("hello");
     }
 );
+
+
 
 
