70e1b8032f5bccc6529c2bf147bedb3a77e467cb
jon <programjm@gmail.com>
2019-01-02 19:18:40 -0600

    Fix async. storage methods and add update() function and STATE enum to Site.js

--- public/Site.js
+++ public/Site.js
@@ -1,6 +1,16 @@
 // @flow
+import { SITES } from './constants';
 
-
+/**
+ * An enum that describes the state of the Site
+ */
+const STATES = Object.freeze({
+    UNBLOCKED: 'unblocked', // unblock w/o interrupt window
+    UNBLOCKED_INTERRUPT: 'unblocked_interrupt', //unblocked, but the user is being interrupted by an intermediate timer
+    LOCKDOWN: 'lockdown',
+    BLOCKED: 'blocked' //the default state
+});
+type State = $Values<typeof STATES>;
 
 
 //Blocked site class
@@ -20,44 +30,107 @@ export default class Site {
     lastTimeUpdate: Date;
 
     /**
-     * Get the entire list of Site objects from disk
-     * @returns An array of all the stored Sites
+     * Updates the internal timing system of the Site.
+     * If wasActive is true, i.e. the site was active, the remaing times until unblock disruptions decrease
+     * timesUntilUnblockDisruptions may be left with zeroes (but not negative values)
+     * It is up to the caller to query the state of the Site and
+     * then later pop these values after the user has passed the interruption.
+     * This function also subtracts from the lockdown timer if applicable
+     * @param wasActive Whether the Site was active just before calling the function.
      */
-    static async getBlockedSites() : ?Site[] {
-        let sites;
-        chrome.storage.local.get([SITES], store => { sites = store.sites; })
-        return await sites;
-    }
+    update(wasActive: boolean) : void {
+        // The time to subtract is calculated as the time since the last update, if applicable.
+        const totalTimeToSubtract : number = this.lastTimeUpdate ? Date.now() - this.lastTimeUpdate : 0;
 
-    /**
-     * Get a Site from disk matching a given domain.
-     * @returns The matching Site or null if the domain is not found
-     */
-    static getSite(domain) : ?Site {
-        const sites : ?Site[]  = getBlockedSites();
-        if (sites != null)
-            return sites.find( site => site.domain === domain);
-        else return null;
-    }
+        // Subtract from the unblock interrupt times, if applicable.
+        let unblockTimeSubtracted : number = 0;
+        if (wasActive)
+        {
+            for (let i=0; i<this.timesUntilUnblockDisruptions.length; ++i)
+            {
+                const initial = this.timesUntilUnblockDisruptions[i];
+                const subtracted = initial > totalTimeToSubtract ? totalTimeToSubtract : initial;
 
-    /**
-     * Store the Site in disk.
-     * If a Site w/ same domain is found, replaces it.
-     * @param site Site to put in storage
-     */
-    function putSite(site: Site) : void {
-        const sites : Site[] = getBlockedSites() || [];
-        let replaceIndex : number = sites.length; //by default, if the Site is not already in the array, it is added to the end
-        //change replaceIndex to the index of the existing Site if applicable
-        if (sites) {
-            const foundIndex : number = sites.findIndex( s => s.domain === site.domain);
-            if (foundIndex !== -1)
-                replaceIndex = foundIndex;
+                this.timesUntilUnblockDisruptions[i] -= subtracted;
+
+                unblockTimeSubtracted = Math.max(unblockTimeSubtracted, subtracted);
+            }
         }
 
-        //Do the replacement and store the array.
-        sites[replaceIndex] = site;
-        chrome.storage.local.set({ SITES, sites });
+        // Use the remaining time differfence to subtract from the lockdown time.
+        const lockdownSubtractTime = Math.min(totalTimeToSubtract - unblockTimeSubtracted, this.lockdownRemainingTime);
+        if (this.lockdownRemainingTime > 0)
+            this.lockdownRemainingTime -= lockdownSubtractTime;
+
+
+        // Finally, update the last update time to now
+        this.lastTimeUpdate = new Date();
+
     }
+
+    calculateCurrentState() : State {
+        // The time since last update
+        const delta : number = new Date() - this.lastTimeUpdate;
+
+        const initialInterruptTime : number = (this.timesUntilUnblockDisruptions[0] - delta) || 0;
+        const finalReblockTime : number = (this.timesUntilUnblockDisruptions[this.timesUntilUnblockDisruptions.length - 1] - delta) || 0;
+        const lockdownTime : number = this.lockdownRemainingTime - delta || 0;
+
+        if (finalReblockTime > 0) { //if still unlocked
+            if (initialInterruptTime <= 0) //interrupt now?
+                return STATES.UNBLOCKED_INTERRUPT;
+            else
+                return STATES.UNBLOCKED;
+        }
+        else if (lockdownTime > 0) //not unlocked; still in lockdown?
+            return STATES.LOCKDOWN;
+        else
+            return STATES.BLOCKED; //default state
+    }
+
 }
 
+
+
+
+/**
+ * Get the entire list of Site objects from disk
+ * @returns A promise that returns to an array of all the stored Sites
+ */
+function getBlockedSites() : Promise<?Site[]> {
+    return new Promise(
+        resolve => {
+            chrome.storage.local.get(SITES, store => resolve(store.sites));
+        }
+    );
+}
+
+/**
+ * Get a Site from disk matching a given domain.
+ * @returns A promise that will resolve to the matching Site or null if the domain is not found
+ */
+export function getSite(domain : string) : Promise<?Site> {
+    return getBlockedSites().then(
+        sites => sites != null ? sites.find(site => site.domain === domain) : null
+    );
+}
+
+/**
+ * Store the Site in disk.
+ * If a Site w/ same domain is found, replaces it.
+ * @param site Site to put in storage
+ */
+export async function putSite(site: Site) {
+    const sites = await getBlockedSites() || [];
+    let replaceIndex : number = sites.length; //by default, if the Site is not already in the array, it is added to the end
+    //change replaceIndex to the index of the existing Site if applicable
+    if (sites) {
+        const foundIndex : number = sites.findIndex( s => s.domain === site.domain);
+        if (foundIndex !== -1)
+            replaceIndex = foundIndex;
+    }
+
+    //Do the replacement and store the array.
+    sites[replaceIndex] = site;
+    chrome.storage.local.set({ SITES, sites });
+}
