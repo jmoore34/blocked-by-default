558022823120d917bbe2dfbfe7fd7dccd5bafb0a
jon <programjm@gmail.com>
2019-01-10 14:58:47 -0600

    Add getStateFromURL function

---
+++ public/util.js
@@ -0,0 +1,15 @@
+// @flow
+import type State from './Site';
+import {getSite} from "./Site";
+
+export function getDomain(url: string) : string {
+    return new URL(url).hostname;
+}
+
+export async function getStateFromURL(url: string) : Promise<?State> {
+    const domain : string = getDomain(url);
+    getSite(domain).then( site => {
+        if (site)
+            return site.calculateCurrentState();
+    });
+}
